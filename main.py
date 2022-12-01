4ebc2e7e1611880fbf11dd46d9d9d659eceea17a
Maxim345525 <maximmaksimchyk@gmail.com>
2022-12-01 21:17:27 +0200

    Init

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-try:
-    size = int(input("Введіть число:"))
-    begin = -50
-    end = 50
-    my_text = []
-    for j in range(size):
-        my_text.append(random.randint(begin, end))
+import random
+start = int(input("start->"))
+finish = int(input("finish->"))
+size = int(input("size->"))
+list = []
 
+for i in range(size):
+    list.append(random.randint(start, finish))
 
+list.sort()
+for item in list:
+    print(item, end=" ")
+print()
 
-
-    random.shuffle(my_text)
-    print(my_text)
-    data = {1: lambda lst: max(lst), 2: lambda lst: min(lst)}
-    print(data[1](my_text))
-except Exception as ex:
-    print(ex)
-
-
+data = {1: lambda lst: max(lst), 2: lambda lst: min(lst)}
+print(data[1](list))
+print(data[2](list))
